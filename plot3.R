921af8f8c983b20f906a1ad6055ac8a18b24962a
ryhpez830 <ryhpez830@gmail.com>
2017-07-23 01:22:00 +0900

    assignment1

---
+++ plot3.R
@@ -0,0 +1,21 @@
+read.csv("household_power_consumption.txt",
+ sep=";",header=T,na.strings="?")->data
+library(dplyr)
+filter(data,Date=="1/2/2007" |
+ Date=="2/2/2007")->data
+library(lubridate)
+dmy(data$Date)->data$Date
+
+attach(data)
+ymd_hms(paste(Date,Time))->datetime
+
+plot(datetime,Sub_metering_1,type="l",
+ xlab="",ylab="Energy sub metering")
+points(datetime,Sub_metering_2,col="red",type="l")
+points(datetime,Sub_metering_3,col="blue",type="l")
+legend("topright",paste("Sub_metering",1:3,sep="_"),
+ col=c("black","red","blue"),lty=1)
+
+detach(data)
+
+ 
